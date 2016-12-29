3ac93d5fbe4ae13d3ed7beebf594b84b5a4909aa
Mike Gunderloy <MikeG1@larkfarm.com>
2016-12-29 05:44:16 -0600

    Add code coverage check

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,7 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'faria/launchpad/api'
 
+require "simplecov"
+SimpleCov.start
+
+require 'faria/launchpad/api'
 require 'minitest/autorun'
