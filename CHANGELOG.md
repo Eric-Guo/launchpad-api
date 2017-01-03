29d5ed07efb437dac1adf477241e9d8ccb838c88
Mike Gunderloy <MikeG1@larkfarm.com>
2017-01-03 14:08:40 -0600

    Prepare 0.2.2 release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,7 @@
 ## 0.2.1 - 26 September 2016
 
 - First public release
+
+## 0.2.2 - 3 January 2017
+
+- Updated JWE/JWT dependencies
--- lib/faria/launchpad/api/version.rb
+++ lib/faria/launchpad/api/version.rb
@@ -1,7 +1,7 @@
 module Faria
   module Launchpad
     module Api
-      VERSION = "0.2.1"
+      VERSION = "0.2.2"
     end
   end
 end
